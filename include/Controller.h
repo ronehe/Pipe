1b092e7653c838a5f2c342ccbe6babbc2edd0353
ronehe <Ro.Heifetz@gmail.com>
2021-05-13 20:39:16 +0300

    slight code visibility changes

--- include/Controller.h
+++ include/Controller.h
@@ -13,19 +13,11 @@ class Controller {
 public:
 	Controller();
 	void startGame();//the game loop
-
-
-
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~private functions~~~~~~~~~~//
 private:
-	
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~private members~~~~~~~~~~//
-
 private:
 	sf::RenderWindow m_gameWindow;
 	sf::Sprite m_background;
 	Board m_board;
-
-	//std::make_unique<<std::vector<Tool*>>m_Chess_Pieces[2];
-	//std::vector<std::vector<Tool*>>m_Chess_Pieces[2];
 };
--- src/Textures.cpp
+++ src/Textures.cpp
@@ -2,30 +2,24 @@
 #include "Macros.h"
 #pragma once
 Textures& Textures::instance() {
-	static Textures  all_S;
+	static Textures all_S;
 	return all_S;
 }
+
 //set textures for all object in the game once
+//all textures created here in a temporary variable, and are inserted into the map using copy constructors
 Textures::Textures() {
-	//m_Font.loadFromFile("Love America.ttf");//setting one font
 	sf::Texture pic;
-	sf::Image im;
-	im.loadFromFile("pieces.png");
+
 	pic.loadFromFile("curvedPipe - Copy.png");
 	pic.setSmooth(true);
 	m_pipeTex.insert(std::pair<pipeTextures,sf::Texture>(curvedPipe,pic));
 
 	pic.loadFromFile("straightPipe - Copy.png");
 	pic.setSmooth(true);
-	
 	m_pipeTex.insert(std::pair<pipeTextures, sf::Texture>(straightPipe, pic));
 
 	pic.loadFromFile("background.jpeg");
+	pic.setSmooth(true);
 	m_pipeTex.insert(std::pair<pipeTextures, sf::Texture>(background, pic));
-
-
-
-
-
-
 }
